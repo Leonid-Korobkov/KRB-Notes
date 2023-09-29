940972fb9dcbd63809298c1ab067859ab4ecc7ad
Leonid-Korobkov <korobkov-leo@mail.ru>
2023-09-29 20:12:26 +0300

    add firebase config to production

---
+++ src/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAF4iI6wQ90HVoMs4-5t1TAZnG8uYyDxEw',
+  authDomain: 'krb-notes.firebaseapp.com',
+  projectId: 'krb-notes',
+  storageBucket: 'krb-notes.appspot.com',
+  messagingSenderId: '618563792282',
+  appId: '1:618563792282:web:91508b06d0c0d468e85e23'
+}
